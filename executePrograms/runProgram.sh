c33607dfa3a14ef96c4f4ee030db04c934c6d5d4
wasartin <wasartin@iastate.edu>
2020-05-29 15:57:18 -0500

    Ran Ouroboros. it works and I added a readme

--- executePrograms/runProgram.sh
+++ executePrograms/runProgram.sh
@@ -4,28 +4,47 @@ CONTINUE=true
 
 function isFileAccessible(){
     input_file=$@
-    [[ -f ${input_file}.java ]] && [[ -x ${input_file}.java ]]
+    if [[ -f ${input_file}.java ]]; then
+      return 0
+    else
+      return 1
+    fi
 }
 
+function isExtensionIncluded(){
+file_name=$@
+  if [[ $file_name == *".java" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 if [ "$#" = "0" ]; then
     read -p "Please give a file_name to replicate: " INPUT_FILE
+    read -p "Please input a number for iteration cycles: " ITER_CYCLE
+elif [ "$#" = "1" ]; then
+    INPUT_FILE="$1"
     read -p "Please input a number for iteration cycles: " ITER_CYCLE
 else
     INPUT_FILE="$1"
     ITER_CYCLE="$2"
 fi
-echo  "${INPUT_FILE}.java" 
+
 # reset/cleanup
 rm -f "${INPUT_FILE}_result.txt"
 rm -f "${INPUT_FILE}.class"
-if [ $isFileAccessible $INPUT_FILE ]; then
+if [[ $(isExtensionIncluded $INPUT_FILE; echo $?) -eq 0 ]]; then
+  INPUT_FILE=${INPUT_FILE/%".java"}
+fi
+
+if [[ $(isFileAccessible $INPUT_FILE; echo $?) -eq 0 ]]; then
 # if [ -x ${INPUT_FILE}.java ]; then
-    echo "File Found"
+    echo "File Found - ${INPUT_FILE}.java"
     # optionally watch the result.txt file with "tail -f result.txt"
     while $CONTINUE; do
 	COUNTER=$[COUNTER+1]
-	echo "Counter:= " $COUNTER
+	echo "Iteration:= " $COUNTER
 	rm -f "${INPUT_FILE}.class" # remove executable
 	javac "${INPUT_FILE}.java"  # make a new executable
 	rm -f "${INPUT_FILE}.java"  # remove source file
@@ -36,7 +55,7 @@ if [ $isFileAccessible $INPUT_FILE ]; then
 	# execute input file, append/create results to *_result.txt,
 	java ${INPUT_FILE} 2>> "${INPUT_FILE}_result.txt" 1>> "${INPUT_FILE}.java"
 	if [ $((COUNTER%${ITER_CYCLE})) -eq 0 ]; then
-	    read -p "Do you wish to continue? y/n:" answer
+	    read -p "Do you want to continue? [y/n] " answer
 	else
 	    answer="$@"
 	fi
@@ -45,5 +64,5 @@ if [ $isFileAccessible $INPUT_FILE ]; then
 	fi
     done
 else
-    echo "File Not Found"
+    echo "File Not Found -  ${INPUT_FILE}.java"
 fi
